4ecae5410d01855271dfa36c597dcab331fd76f4
Serhat SARI <serhats@Serhats-MacBook-Pro.local>
2012-09-12 17:47:34 +0300

    Initial Commit

---
+++ NetmeraMediaTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NetmeraMediaTest
+//
+//  Created by Serhat SARI on 9/12/12.
+//  Copyright (c) 2012 Netmera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NetmeraMediaTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NetmeraMediaTest
+//
+//  Created by Serhat SARI on 9/12/12.
+//  Copyright (c) 2012 Netmera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
